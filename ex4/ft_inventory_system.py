83ef439a146de81d18b033d9387c79858c717aad
LucasBods <lucasferreira.bods@gmail.com>
2026-09-18 00:15:25 -0300

    ex3 ok, ex4 wip

---
+++ ex4/ft_inventory_system.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+inventory = {}
+
+
+def parse(args: list[object]) -> None:
+    
+
+
+if __name__ == "__main__":
+    print("=== Inventory System Analysis ===")
+
+    args = sys.argv[1:]
